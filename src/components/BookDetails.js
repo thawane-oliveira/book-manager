41152d63dd06161c3a2d27cf7e8615111719f55d
Thawane <thawaneapoliveira@gmail.com>
2023-06-17 01:06:09 -0300

    styles: implementa estilização no modal, respeitando a responsividade em telas menores

--- src/components/BookDetails.js
+++ src/components/BookDetails.js
@@ -8,18 +8,32 @@ function BookDetails({ description, thumbnail, title, author }) {
   };
 
   return (
-    <div className="flex justify-center items-center">
+    <div className="flex justify-center items-center w-full h-full">
       {!showDetail &&
         <div
-          className="flex flex-col justify-center items-center overflow-x-hidden overflow-y-auto
-         fixed inset-0 z-50 outline-none focus:outline-none w-100 backdrop-blur-md"
+          className="flex flex-col justify-center items-center fixed inset-0 
+          z-50 outline-none focus:outline-none w-100 backdrop-blur-md"
         >
-          <div className="w-3/4  bg-amber-50">
-            <h1>Detalhes do livro</h1>
-            <img src={thumbnail} alt={title} />
-            <h2>{title}</h2>
-            <p>{author}</p>
-            <p>{description}</p>
+          <div className="w-4/5 bg-amber-50 overflow-auto h-full px-[1%]
+          flex flex-col items-center justify-between py-[2%] xs:h-4/5 xs:w-3/5
+          rounded-lg"
+          >
+            <h1 className="text-center font-extrabold text-red-900 font-serif text-lg">
+              Detalhes do livro
+            </h1>
+            <img
+              src={thumbnail}
+              alt={title}
+            />
+            <h2 className="text-center font-extrabold font-serif text-lg">
+              {title}
+              </h2>
+            <p className="text-center font-semibold font-serif text-md">
+              {author}
+              </p>
+            <p className="text-center font-normal font-serif text-md">
+              {description}
+              </p>
             <button
               onClick={seeBookDetails}
               type="button"
